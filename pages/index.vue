050f3581a0dd251be1eb3fce51a5fe67aaa47814
Stanislav Klymoshenko <stanislav.klymoshenko@easysoftware.com>
2020-11-30 20:01:49 +0100

    set up mobile view

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="container h-screen">
+  <div class="container md:h-screen relative">
     <MainTitle />
     <div
       class="started-test flex flex-col md:flex-row mx-auto border-secondary border-solid border-2 w-4/5 z-10 origin-center"
     >
-      <GetStarted @from:changed="handleFromChange" @to:changed="handleToChange" />
+      <GetStarted
+        @from:changed="handleFromChange"
+        @to:changed="handleToChange"
+      />
       <Chat :to="to" :from="from" />
     </div>
+    <img
+      src="../static/undraw_conversation_h12g.svg"
+      alt="#"
+      class="backround-image w-6/12 md:w-3/12 absolute top-2 left-1 md:top-2 md:left-3"
+    >
+    <img
+      src="../static/undraw_super_thank_you_obwk.svg"
+      alt="#"
+      class="backround-image w-6/12 md:w-3/12 absolute top-2 right-1 md:top-2 md:right-5"
+    >
+    <img
+      src="../static/undraw_share_opinion_jpw0.svg"
+      alt="#"
+      class="backround-image w-6/12 md:w-3/12 absolute bottom-2 right-4 md:bottom-2 md:right-15"
+    >
   </div>
 </template>
 
@@ -19,7 +37,6 @@ import Chat from "~/components/Chat.vue";
 @Component({
   components: { MainTitle, GetStarted, Chat }
 })
-
 export default class Index extends Vue {
   from: string = "Mike";
   to: string = "My lovely Boss";
@@ -31,7 +48,13 @@ export default class Index extends Vue {
   handleToChange(to: string): void {
     this.to = to;
   }
-};
+}
 </script>
 
-<style></style>
+<style>
+@media (max-width: 640px) {
+  .container {
+    height: 1000px !important;
+  }
+}
+</style>
